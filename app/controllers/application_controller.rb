6da08731cf76fa3917bb64dcb593a1af70ecf8f1
Aris BUZACHIS <aris-antonio.buzachis@ubisoft.com>
2013-11-06 18:28:11 +0200

    Moved CORS headers into modules.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,2 @@
 class ApplicationController < ActionController::API
-  before_filter :protect
-  before_filter :allow_cors
-
-  def allow_cors
-	headers["Access-Control-Allow-Origin"] = "*"
-	headers["Access-Control-Allow-Methods"] = %w{GET POST PUT PATCH DELETE}.join(",")
-	headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-
-	head(:ok) if request.request_method == "OPTIONS"
-  end
-
-  def protect
-    @ips = ['127.0.0.1']
-    if not @ips.include? request.remote_ip
-      head :forbidden
-      return
-    end
-  end
 end
